551a1806be3f6f40e7c0f0c3595c3191407bcb93
justdupp <hqxhj1990@163.com>
2018-04-21 00:29:11 +0800

    配置文件提交

---
+++ cost-center/src/main/java/com/hecc/costcenter/CostCenterApplication.java
@@ -0,0 +1,12 @@
+package com.hecc.costcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostCenterApplication.class, args);
+	}
+}
